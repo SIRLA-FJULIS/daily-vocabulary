8a9d5c3ea3006b0c2dcdad9d6d0650593016a06a
bessyhuang <bessyhuang1@gmail.com>
2023-02-05 11:53:26 +0800

    [!] DB & Table connection

--- manage/lib/get_all_voc.php
+++ manage/lib/get_all_voc.php
@@ -2,7 +2,7 @@
 	$DATABASE_HOST = 'localhost';
 	$DATABASE_USER = 'root';
 	$DATABASE_PASS = '';
-	$DATABASE_NAME = 'daily_voc';
+	$DATABASE_NAME = 'daily-vocabulary';
     
 	// Try and connect using the info above.
 	$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
--- manage/lib/insert_voc.php
+++ manage/lib/insert_voc.php
@@ -3,7 +3,7 @@
 	$DATABASE_HOST = 'localhost';
 	$DATABASE_USER = 'root';
 	$DATABASE_PASS = '';
-	$DATABASE_NAME = 'daily_voc';
+	$DATABASE_NAME = 'daily-vocabulary';
     
 	// Try and connect using the info above.
 	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
